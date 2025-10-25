5a4aba051350c6839c8615f4daffb78423061ddb
tayarinina-collab <tayari.nina@gmail.com>
2025-10-26 00:08:16 +0200

    Add sequence management module integration

---
+++ AI_Primer_Design_Pro_2_0/app/modules/main.py
@@ -0,0 +1,6 @@
+from modules.sequence_management import run_sequence_management
+
+# Beispiel: Wenn du Klick-Navigation hinzufügst
+selected_module = "Sequence Management"
+if selected_module == "Sequence Management":
+    run_sequence_management()
